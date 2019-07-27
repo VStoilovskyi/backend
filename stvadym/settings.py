5b045dfaceb06f52d82b1a02e6612614ee09d3fd
VStoilovskyi <vadimstoylovskiy@gmail.com>
2019-07-27 21:49:12 +0300

    Another attempt STACIC ROUTE update

--- stvadym/settings.py
+++ stvadym/settings.py
@@ -133,4 +133,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+STATIC_ROUTE = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
